cd54a00b4c14af1de2c170a8035f52146a2ecf51
Cheng Gu <guchengf@gmail.com>
2018-05-18 13:27:35 +0800

    skip YCM in neovim

--- install.sh
+++ install.sh
@@ -78,17 +78,17 @@ export SHELL="/bin/sh"
 if $FOR_VIM; then
     vim -u $HOME/.vimrc.bundles +PlugInstall! +PlugClean! +qall
 else
-    nvim -u $HOME/.vimrc.bundles +PlugInstall! +PlugClean! +qall
+    nvim -u $HOME/.config/nvim/vimrc.bundles +PlugInstall! +PlugClean! +qall
 fi
 export SHELL=$system_shell
 
-
-echo "Step4: compile YouCompleteMe"
-echo "It will take a long time, just be patient!"
-echo "If error,you need to compile it yourself"
-echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && python install.py --clang-completer"
-cd $CURRENT_DIR/bundle/YouCompleteMe/
-git submodule update --init --recursive
+if $FOR_VIM; then
+    echo "Step4: compile YouCompleteMe"
+    echo "It will take a long time, just be patient!"
+    echo "If error,you need to compile it yourself"
+    echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && python install.py --clang-completer"
+    cd $CURRENT_DIR/bundle/YouCompleteMe/
+    git submodule update --init --recursive
 if [ `which clang` ]   # check system clang
 then
     python install.py --clang-completer --system-libclang   # use system clang
