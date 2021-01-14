077dcbfe1b2175f2b32cf176209c0c9a49de54a6
gucheen <guchengf@gmail.com>
2021-01-14 17:32:42 +0800

    feat: 更新vim设置

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 # refer  spf13-vim bootstrap.sh`
 BASEDIR=$(dirname $0)
@@ -81,19 +81,5 @@ else
     nvim -u $HOME/.config/nvim/vimrc.bundles +PlugInstall! +PlugClean! +qall
 fi
 export SHELL=$system_shell
-
-if $FOR_VIM; then
-    echo "Step4: compile YouCompleteMe"
-    echo "It will take a long time, just be patient!"
-    echo "If error,you need to compile it yourself"
-    echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && python install.py --clang-completer"
-    cd $CURRENT_DIR/bundle/YouCompleteMe/
-    git submodule update --init --recursive
-if [ `which clang` ]   # check system clang
-then
-    python install.py --clang-completer --system-libclang   # use system clang
-else
-    python install.py --clang-completer
-fi
 
 echo "Install Done!"
